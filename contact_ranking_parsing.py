d7227c324bd18164a95d724ddf2e279258a5a9f0
upupsheep <sandy861003@gmail.com>
2019-12-25 17:22:18 +0800

    push main code

---
+++ contact_ranking_parsing.py
@@ -0,0 +1,65 @@
+import re
+import csv
+"""
+Total 13 column: 
+['?色', '文本名', '代?', '重量', '部分', '作者', '?建日期', '批注',	'文本?', '?域',	'覆?率 %', '起?', '??\n']
+
+Encoding by: big5hkscs
+"""
+
+in_file = open("contact_ranking.txt", "r", encoding="big5hkscs")
+out_file = open("cr_parsing.txt", "w", encoding="big5hkscs")
+
+line = in_file.readline()
+# print(line)
+
+code_dict = {}
+buffer = []
+while line:
+    str_split = re.split(r'\t+', line)
+    # print(str_split)
+    if len(str_split) > 1 and str_split[0] != '?色':
+        # print(str_split[0])
+        if str_split[0] == '●':
+            # print('yes!!!')
+            buffer = str_split
+        else:
+            buffer.extend(str_split)
+
+        print(buffer)
+        print(len(buffer))
+        code_name = buffer[2]
+        content = buffer[4]
+
+        if code_name not in code_dict:
+            code_dict[code_name] = list()
+        code_dict[code_name].append([content])
+        # print(code_dict)
+
+        # out_file.write("\n".join(buffer))
+    line = in_file.readline()
+
+# with open('cr_parsing.txt', 'w', encoding='big5hkscs') as file:
+# out_file.write(str(code_dict))  # use `json.loads` to do the reverse
+# with open("output.csv", 'w') as resultFile:
+#     wr = csv.writer(resultFile, dialect='excel')
+#     wr.writerows(code_dict)
+
+w = csv.writer(open("output.csv", "w", encoding="big5hkscs"))
+for key, val in code_dict.items():
+    w.writerow([key, val])
+'''
+dictlist = []
+for key, value in code_dict.items():
+    temp = [key, value]
+    dictlist.append(temp)
+print(dictlist)
+'''
+'''
+with open("output.csv", "w", encoding="big5hkscs") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(code_dict.keys())
+    writer.writerows(zip(*code_dict.values()))
+'''
+in_file.close()
+out_file.close()
