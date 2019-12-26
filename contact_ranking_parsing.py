26939cbecabff37de2003e8b5c86eab6e850347c
upupsheep <sandy861003@gmail.com>
2019-12-26 10:35:33 +0800

    add comment

--- contact_ranking_parsing.py
+++ contact_ranking_parsing.py
@@ -1,57 +1,46 @@
 import re
 import csv
 """
-Total 13 column: 
+- Total 13 column: 
 ['?色', '文本名', '代?', '重量', '部分', '作者', '?建日期', '批注',	'文本?', '?域',	'覆?率 %', '起?', '??\n']
 
-Encoding by: big5hkscs
+- Encoding by: big5hkscs
 """
+### Read Input File ###
 filename = "contact_ranking.txt"
 in_file = open(filename, "r", encoding="big5hkscs")
-
 line = in_file.readline()
 # print(line)
 
+### Read by Line into a list ###
 code_dict = {}
 buffer = []
 while line:
-    str_split = re.split(r'\t+', line)
-    # print(str_split)
+    str_split = re.split(r'\t+', line)  # Split by tab
+
+    ### length > 1 means there's a code. ###
+    ### But the first line must be excluded. ###
     if len(str_split) > 1 and str_split[0] != '?色':
-        # print(str_split[0])
         if str_split[0] == '●':
-            # print('yes!!!')
             buffer = str_split
-        else:
+        else:  # accedently cutted, so append it to the last list
             buffer.extend(str_split)
 
         print(buffer)
         print(len(buffer))
         code_name = buffer[2]
         content = buffer[4]
-
+        ### Put the buffer into the dictionary ###
         if code_name not in code_dict:
             code_dict[code_name] = list()
         code_dict[code_name].append([content])
         # print(code_dict)
 
-        # out_file.write("\n".join(buffer))
-    line = in_file.readline()
+    line = in_file.readline()  # read next line
 
+### Write the dictionary into a csv file ###
 w = csv.writer(open("output.csv", "w", encoding="big5hkscs"))
 for key, val in code_dict.items():
     w.writerow([key, val])
-'''
-dictlist = []
-for key, value in code_dict.items():
-    temp = [key, value]
-    dictlist.append(temp)
-print(dictlist)
-'''
-'''
-with open("output.csv", "w", encoding="big5hkscs") as outfile:
-    writer = csv.writer(outfile)
-    writer.writerow(code_dict.keys())
-    writer.writerows(zip(*code_dict.values()))
-'''
+
 in_file.close()
